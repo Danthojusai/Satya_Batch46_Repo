6142cebc53d94111b3114b5ac8589b62e7871ad2
saikumar <danthojusaikumar@gmail.com>
2021-12-07 08:07:05 +0530

    modified JIRA-101

--- src/java/test.java
+++ src/java/test.java
@@ -3,7 +3,9 @@ class test {
  public static void main(String args[])  
 {    
     
- System.out.print (test sample java prm)   
+ System.out.print (test sample java prm) 
+ System.out.print (test sample java 2) 
     
  
 } 
+}
